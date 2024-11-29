12c1f319e540919895dec4974461dc4d4fea1ff5
Lotoz <zamira.grippi@gmail.com>
2024-11-29 14:44:48 +0100

    z

--- Practica Personal/prog.unidad03.combinado.TemperaturaClasificacionApp/src/prog/unidad03/combinado/TemperaturaClasificacionApp/TemperaturaClasificacionApp.java
+++ Practica Personal/prog.unidad03.combinado.TemperaturaClasificacionApp/src/prog/unidad03/combinado/TemperaturaClasificacionApp/TemperaturaClasificacionApp.java
@@ -5,34 +5,35 @@ import java.util.Scanner;
 
 public class TemperaturaClasificacionApp {
 
-	public static void main(String[] args) { 
-		
-		boolean salida = false;
-		do {
-			try {
-				Scanner sc = new Scanner(System.in);
-				
-				System.out.println("Introduce la temperatura:");
-				double temperatura = Double.parseDouble(sc.nextLine());
+  public static void main(String[] args) {
 
-				if (temperatura < 0) {
-					System.out.printf("La temperatura es fria.%n");
-				} else if (temperatura < 15) {
-					System.out.printf("La temperatura es fresca.%n");
-				} else if (temperatura < 25) {
-					System.out.printf("Es templado.%n");
-				} else {
-					System.out.printf("Hace calo'.%n");
-				}
-				
-				double fahrenheit = temperatura * 59 + 32;
-				System.out.printf(Locale.US,"La temperatura introducida es %f y en fahrenheit es %f.%n", temperatura, fahrenheit);
-				
-				salida = true;
-			} catch (NumberFormatException e) {
-				System.out.printf("Error.%n");
-				
-			} 
-		} while (!salida);
-	} 
+    boolean salida = false;
+    do {
+      try {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce la temperatura:");
+        double temperatura = Double.parseDouble(sc.nextLine());
+
+        if (temperatura < 0) {
+          System.out.printf("La temperatura es fria.%n");
+        } else if (temperatura < 15) {
+          System.out.printf("La temperatura es fresca.%n");
+        } else if (temperatura < 25) {
+          System.out.printf("Es templado.%n");
+        } else {
+          System.out.printf("Hace calo'.%n");
+        }
+
+        double fahrenheit = temperatura * 59 + 32;
+        System.out.printf(Locale.US, "La temperatura introducida es %f y en fahrenheit es %f.%n", temperatura,
+            fahrenheit);
+
+        salida = true;
+      } catch (NumberFormatException e) {
+        System.out.printf("Error.%n");
+
+      }
+    } while (!salida);
+  }
 }
