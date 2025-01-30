483bc5182bffa31e22194e529f860d70e5178d8c
Lotoz <zamira.grippi@gmail.com>
2025-01-30 14:27:15 +0100

    30/01-25

--- TaskManager/src/TaskManagerJsonReader/LeerJson.java
+++ TaskManager/src/TaskManagerJsonReader/LeerJson.java
@@ -2,4 +2,10 @@ package TaskManagerJsonReader;
 
 public class LeerJson {
 
+  private String nombreFichero;
+  
+  public LeerJson(String nombreFichero) {
+    this.nombreFichero = nombreFichero;
+  }
+  
 }
