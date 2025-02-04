758087616d39d8012b223d723923d7681b5b2798
Lotoz <zamira.grippi@gmail.com>
2025-02-04 20:31:01 +0100

    .

--- prog.ud05.actividad511/src/prog/ud05/actividad511/proveedores/ProveedorUsuariosArchivoJSON.java
+++ prog.ud05.actividad511/src/prog/ud05/actividad511/proveedores/ProveedorUsuariosArchivoJSON.java
@@ -2,7 +2,7 @@ package prog.ud05.actividad511.proveedores;
 
 public class ProveedorUsuariosArchivoJSON {
 
-  public ProveedorUsuariosArchivoJSON() {
+  public ProveedorUsuariosArchivoJSON(String archivo) {
     // TODO Auto-generated constructor stub
   }
 
